5e31d172652094db80738d2bfc676a5ab1945995
Johnson Ho <kcho@wifi-interno.fe.up.pt>
2018-02-15 13:40:04 +0000

    add wall detect

--- Dungeon Keep/src/code/Main.java
+++ Dungeon Keep/src/code/Main.java
@@ -4,24 +4,31 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static String[][] map= {{"X","X","X","X","X","X","X","X","X","X"},
-			 		 {"X","H"," "," ","I"," ","X"," ","G","X"},
-			 		 {"X","X","X"," ","X","X","X"," "," ","X"},
-			 		 {"X"," ","I"," ","I"," ","X"," "," ","X"},
-			 		 {"X","X","X"," ","X","X","X"," "," ","X"},
-			 		 {"I"," "," "," "," "," "," "," "," ","X"},
-			 		 {"I"," "," "," "," "," "," "," "," ","X"},
-			 		 {"X","X","X"," ","X","X","X","X"," ","X"},
-			 		 {"X"," ","I"," ","I"," ","X"," "," ","X"},
-			 		 {"X","X","X","X","X","X","X","X","X","X"}};
-	static String[][] copy=map;
+	static String[][] map = {
+				{"X","X","X","X","X","X","X","X","X","X"},
+				{"X","H"," "," ","I"," ","X"," ","G","X"},
+				{"X","X","X"," ","X","X","X"," "," ","X"},
+				{"X"," ","I"," ","I"," ","X"," "," ","X"},
+				{"X","X","X"," ","X","X","X"," "," ","X"},
+				{"I"," "," "," "," "," "," "," "," ","X"},
+				{"I"," "," "," "," "," "," "," "," ","X"},
+				{"X","X","X"," ","X","X","X","X"," ","X"},
+				{"X"," ","I"," ","I"," ","X"," "," ","X"},
+				{"X","X","X","X","X","X","X","X","X","X"}
+			};
+
+	static String[][] copy = map;
 	
-	static int positionX=1;
-	static int positionY=1;
+	static int positionX = 1;
+	static int positionY = 1;
 	
 	public static boolean push_remove(String str, int atX, int atY, int byeX, int byeY){
 		
 		if(atX >= 0){
+
+			if(!map[atX][atY].equals(" ")){
+				return false;
+			}
 			// whether there is a wall, do something
 			
 			System.out.print("\nPush: " + atX + ", " + atY);
@@ -35,15 +42,22 @@ public class Main {
 		return true;
 	}
 
-	public static void push(String str, int atX, int atY){
-		boolean can_move= push_remove(str, atX, atY, positionX, positionY );
-		positionX = atX;
-		positionY = atY;
+	public static void moveTo(int toX, int toY){
+		boolean has_moved = push_remove("H", toX, toY, positionX, positionY);
+
+		if(has_moved){
+			positionX = toX;
+			positionY = toY;
+		}
 	}
 
-	public static void remove(int byeX, int byeY){
-		push_remove("", -1, -1, byeX, byeY);
-	}
+	// public static void push(String str, int atX, int atY){
+	// 	push_remove(str, atX, atY, -1, -1);
+	// }
+
+	// public static void remove(int byeX, int byeY){
+	// 	push_remove("", -1, -1, byeX, byeY);
+	// }
 
 	public static void printscreen()
 	{
@@ -88,20 +102,24 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		boolean sair=false;
-		
-		Scanner s= new Scanner(System.in);
-		
+
+		boolean sair = false;
+
 		String input;
+		Scanner s = new Scanner(System.in);
 		
 		while(sair==false)
 		{
 			printscreen();
-			System.out.print("What is the movement you want to make?(W-Up,S-Down,A-Left,D-Right,P-Exit): ");
-			input=s.nextLine();
-			int movement=transformInput(input);
+			System.out.print("What is the movement you want to make? (W-Up, S-Down, A-Left, D-Right, P-Exit): ");
+			input = s.nextLine();
+
+			int movement = transformInput(input);
+
 			if (movement == 0)// exit case
+			{
 				break;
+			}
 			else if (movement == -1)//error state
 			{
 				System.out.println("An error has ocurred during the input. Please provide a valid input.");
@@ -109,18 +127,16 @@ public class Main {
 			}
 			switch(movement)
 			{
-			//moving 
-			case 1:push("H",positionX-1,positionY);
-				
-				break;
-			case 2:push("H",positionX+1,positionY);
-				break;
-			case 3:push("H",positionX,positionY-1);
-				break;
-			case 4:push("H",positionX,positionY+1);
-				break;
+				// moving
+				case 1: moveTo(positionX-1, positionY);
+					break;
+				case 2: moveTo(positionX+1, positionY);
+					break;
+				case 3: moveTo(positionX, positionY-1);
+					break;
+				case 4: moveTo(positionX, positionY+1);
+					break;
 			}
-			
 		}
 		s.close();
 	}
