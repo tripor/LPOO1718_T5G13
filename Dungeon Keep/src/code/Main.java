616c7148a59722ecb3aab51412075c599940a0a0
João Lima <figorifico3000@gmail.com>
2018-02-15 10:59:31 +0000

    main

---
+++ Dungeon Keep/src/code/Main.java
@@ -0,0 +1,12 @@
+package code;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[][] map= {{"X","X","X","X","X","X","X","X","X","X"},
+						 {"X","H"," "," ","I"," ","X"," ","G","X"},
+						 {"X","X","X"," "}};
+
+	}
+
+}
